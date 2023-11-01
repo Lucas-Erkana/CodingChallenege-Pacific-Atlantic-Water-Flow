a0d9679ae0669064d1e92e66d5c9979109551e64
Lucas David Erkana <lderkana21x@gmail.com>
2023-11-01 09:56:30 +0200

    Solution on 1 November 2023

--- solution.js
+++ solution.js
@@ -1,9 +1,68 @@
-var functionname = function(parameter) {
-//Write code here
-  };
+/**
+ * @param {number[][]} heights
+ * @return {number[][]}
+ */
+var pacificAtlantic = function(heights) {
+  const m = heights.length; // Number of rows
+  const n = heights[0].length; // Number of columns
 
+  const result = []; // Array to store the result
 
-  let parameter = 'test data'
-console.log(functionname(parameter)) //expected result true
+  const pacific = Array.from({ length: m }, () => Array(n).fill(false)); // Matrix to track cells reachable from Pacific Ocean
+  const atlantic = Array.from({ length: m }, () => Array(n).fill(false)); // Matrix to track cells reachable from Atlantic Ocean
 
-module.exports = functionname;
+  // DFS function to mark cells as reachable
+  function dfs(row, col, ocean) {
+    ocean[row][col] = true; // Mark the cell as reachable
+
+    // Check neighbors (up, down, left, right)
+    const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    for (const [dx, dy] of directions) {
+      const newRow = row + dx;
+      const newCol = col + dy;
+
+      // Check if the neighbor is within bounds and has not been visited
+      if (
+        newRow >= 0 &&
+        newRow < m &&
+        newCol >= 0 &&
+        newCol < n &&
+        !ocean[newRow][newCol] &&
+        heights[newRow][newCol] >= heights[row][col]
+      ) {
+        dfs(newRow, newCol, ocean); // Recursively visit the neighbor
+      }
+    }
+  }
+
+  // Traverse the first and last rows, marking cells reachable from Pacific and Atlantic oceans
+  for (let col = 0; col < n; col++) {
+    dfs(0, col, pacific); // Traverse the first row
+    dfs(m - 1, col, atlantic); // Traverse the last row
+  }
+
+  // Traverse the first and last columns, marking cells reachable from Pacific and Atlantic oceans
+  for (let row = 0; row < m; row++) {
+    dfs(row, 0, pacific); // Traverse the first column
+    dfs(row, n - 1, atlantic); // Traverse the last column
+  }
+
+  // Check all cells and add those reachable from both oceans to the result array
+  for (let row = 0; row < m; row++) {
+    for (let col = 0; col < n; col++) {
+      if (pacific[row][col] && atlantic[row][col]) {
+        result.push([row, col]);
+      }
+    }
+  }
+
+  return result;
+};
+
+console.log(pacificAtlantic([
+  [1, 2, 3],
+  [3, 2, 1],
+  [2, 3, 1]
+])) //expected result true
+
+module.exports = pacificAtlantic;
--- tests.js
+++ tests.js
@@ -1,6 +1,57 @@
-const canJump = require('./jump-game');
+const pacificAtlantic = require('./solution');
 const assert = require('assert');
 
 describe('Tests', function () {
-//Write tests here
+  it('Test Case 1', function () {
+    const heights = [
+      [1, 2, 2, 3, 5],
+      [3, 2, 3, 4, 4],
+      [2, 4, 5, 3, 1],
+      [6, 7, 1, 4, 5],
+      [5, 1, 1, 2, 4]
+    ];
+    const expected = [[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]];
+    const result = pacificAtlantic(heights);
+    assert.deepStrictEqual(result, expected);
+  });
+
+  it('Test Case 2', function () {
+    const heights = [
+      [1]
+    ];
+    const expected = [[0, 0]];
+    const result = pacificAtlantic(heights);
+    assert.deepStrictEqual(result, expected);
+  });
+
+  it('Test Case 3', function () {
+    const heights = [
+      [1, 2],
+      [2, 1]
+    ];
+    const expected = [ [ 0, 1 ], [ 1, 0 ] ];
+    const result = pacificAtlantic(heights);
+    assert.deepStrictEqual(result, expected);
+  });
+
+  it('Test Case 4', function () {
+    const heights = [
+      [1, 2, 3],
+      [3, 2, 1],
+      [2, 3, 1]
+    ];
+    const expected = [ [ 0, 1 ], [ 0, 2 ], [ 1, 0 ], [ 1, 1 ], [ 2, 0 ], [ 2, 1 ] ];
+    const result = pacificAtlantic(heights);
+    assert.deepStrictEqual(result, expected);
+  });
+
+  it('Test Case 5', function () {
+    const heights = [
+      [5, 5],
+      [5, 5]
+    ];
+    const expected = [[0, 0], [0, 1], [1, 0], [1, 1]];
+    const result = pacificAtlantic(heights);
+    assert.deepStrictEqual(result, expected);
+  });
 });
